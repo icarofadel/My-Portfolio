4cc8e497514e8f3cd15d0f65d9bf98fbbfa2392f
Icaro <icaro.nata147@gmail.com>
2024-08-25 13:54:27 -0300

    atualizando projetos

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -10,6 +10,9 @@ import {
 } from './styles'
 
 import effod from '../../assets/images/efood.jpg'
+import agenda from '../../assets/images/agendaReact.png'
+import aniver from '../../assets/images/landingpageEvent.png'
+import walle from '../../assets/images/walle.png'
 
 AOS.init()
 
@@ -26,7 +29,7 @@ const Projects = () => (
           <Description>
             <h5>Aplicativo Effod</h5>
             <p>
-              Aplicativo de uma empresa ficticia utilizando React, HookForm,
+              Aplicativo de uma empresa fictícia utilizando React, HookForm,
               Yup, Mask e retornando o número do pedido com uma API. Projeto
               valendo nota pela EBAC.
             </p>
@@ -48,24 +51,23 @@ const Projects = () => (
         </ProjectsItem>
 
         <ProjectsItem>
-          <img src={effod} alt="app efood" />
+          <img src={agenda} alt="Agenda de contato" />
           <Description>
-            <h5>Aplicativo Effod</h5>
+            <h5>Agenda de contato com React</h5>
             <p>
-              Aplicativo de uma empresa ficticia utilizando React, HookForm,
-              Yup, Mask e retornando o número do pedido com uma API. Projeto
-              valendo nota pela EBAC.
+              Agenda de contato utilizando React e Redux. Projeto valendo nota
+              pela EBAC.Valendo nota pela EBAC.
             </p>
             <CampoButton>
               <ButtonLink
                 buttonType="buttonSite"
-                href="https://app-efood.vercel.app/"
+                href="https://projeto-agenda-contato.vercel.app/"
               >
                 Visitar Site
               </ButtonLink>
               <ButtonLink
                 buttonType="buttonRep"
-                href="https://github.com/icarofadel/app_efood"
+                href="https://github.com/icarofadel/projeto-agenda-contato"
               >
                 &lt; Repositorio /&gt;
               </ButtonLink>
@@ -74,24 +76,23 @@ const Projects = () => (
         </ProjectsItem>
 
         <ProjectsItem>
-          <img src={effod} alt="app efood" />
+          <img src={aniver} alt="Landing page - Evento de aniversário" />
           <Description>
-            <h5>Aplicativo Effod</h5>
+            <h5>Landing page - Evento de aniversário</h5>
             <p>
-              Aplicativo de uma empresa ficticia utilizando React, HookForm,
-              Yup, Mask e retornando o número do pedido com uma API. Projeto
-              valendo nota pela EBAC.
+              Landing page de um aniversário utilizando HTML, SCSS, JavaScript e
+              com algumas animações. Projeto valendo nota pela EBAC.
             </p>
             <CampoButton>
               <ButtonLink
                 buttonType="buttonSite"
-                href="https://app-efood.vercel.app/"
+                href="https://event-aniv.vercel.app/"
               >
                 Visitar Site
               </ButtonLink>
               <ButtonLink
                 buttonType="buttonRep"
-                href="https://github.com/icarofadel/app_efood"
+                href="https://github.com/icarofadel/event_aniv"
               >
                 &lt; Repositorio /&gt;
               </ButtonLink>
@@ -100,24 +101,23 @@ const Projects = () => (
         </ProjectsItem>
 
         <ProjectsItem>
-          <img src={effod} alt="app efood" />
+          <img src={walle} alt="Clone Disney+ Filme Wall-e" />
           <Description>
-            <h5>Aplicativo Effod</h5>
+            <h5>Clone Disney+ Filme Wall-e</h5>
             <p>
-              Aplicativo de uma empresa ficticia utilizando React, HookForm,
-              Yup, Mask e retornando o número do pedido com uma API. Projeto
-              valendo nota pela EBAC.
+              Clone da Disney+ para testar algumas habilidades utilizando HTML,
+              JavaScript e SCSS. Projeto valendo nota pela EBAC.
             </p>
             <CampoButton>
               <ButtonLink
                 buttonType="buttonSite"
-                href="https://app-efood.vercel.app/"
+                href="https://disney-plus-wall-e.vercel.app/"
               >
                 Visitar Site
               </ButtonLink>
               <ButtonLink
                 buttonType="buttonRep"
-                href="https://github.com/icarofadel/app_efood"
+                href="https://github.com/icarofadel/disney_plus_wall_e"
               >
                 &lt; Repositorio /&gt;
               </ButtonLink>
